5612fffbd29a2ec7a698460426d928bfa55ff13a
rAmenOLD <roman.nikolaev1997@gmail.com>
2026-09-10 16:49:29 +0400

    chore(repo): remove stale generators and add project readme

--- gen.ps1
+++
@@ -1,6 +0,0 @@
-﻿$dir = "src/components"
-New-Item -ItemType Directory -Force -Path $dir | Out-Null
-function Write-TsxFile {
-  param($path,$content)
-  [System.IO.File]::WriteAllText($path, $content, [System.Text.Encoding]::UTF8)
-}
